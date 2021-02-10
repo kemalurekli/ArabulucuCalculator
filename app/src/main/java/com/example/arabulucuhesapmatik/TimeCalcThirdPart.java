e7286f5c4982750edfb4cf81ad94c511ca6f3cba
Kemal <>
2021-02-11 00:24:55 +0300

    calendar function added

--- app/src/main/java/com/example/arabulucuhesapmatik/TimeCalcThirdPart.java
+++ app/src/main/java/com/example/arabulucuhesapmatik/TimeCalcThirdPart.java
@@ -13,13 +13,18 @@ import java.util.Calendar;
 public class TimeCalcThirdPart extends AppCompatActivity {
 
     private DatePickerDialog.OnDateSetListener setListener;
-    private TextView tvDate, date_text;
+    private TextView tvDate, is_uc, is_dort,tuketici_uc,tuketici_dort,ticari_alti, ticari_sekiz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_calc_third_part);
         tvDate = findViewById(R.id.tvDate);
-        date_text = findViewById(R.id.date_text);
+        is_uc = findViewById(R.id.is_uc);
+        is_dort = findViewById(R.id.is_dort);
+        tuketici_uc = findViewById(R.id.tuketici_uc);
+        tuketici_dort = findViewById(R.id.tuketici_dort);
+        ticari_alti = findViewById(R.id.ticari_alti);
+        ticari_sekiz = findViewById(R.id.ticari_sekiz);
         final Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
@@ -32,13 +37,42 @@ public class TimeCalcThirdPart extends AppCompatActivity {
 
                     @Override
                     public void onDateSet(DatePicker datePicker, int selectedYear, int selectedMonth, int selectedDay) {
-                        Calendar calendarNew = Calendar.getInstance();
-                        calendarNew.set(selectedYear,selectedMonth,selectedDay);
-                        calendarNew.add(calendarNew.DATE,21);
-                        int threeWeekD = (calendarNew.get(Calendar.DAY_OF_MONTH));
-                        int threeWeekM = calendarNew.get(Calendar.MONTH);
+
+                        //For 3 weeks
+                        Calendar calendar2 = Calendar.getInstance();
+                        calendar2.set(selectedYear,selectedMonth,selectedDay);
+                        calendar2.add(calendar2.DATE,21);
+                        int threeWeekD = (calendar2.get(Calendar.DAY_OF_MONTH));
+                        int threeWeekM = calendar2.get(Calendar.MONTH);
                         threeWeekM=threeWeekM+1;
-                        int threeWeekY = calendarNew.get(Calendar.YEAR);
+                        int threeWeekY = calendar2.get(Calendar.YEAR);
+
+                        //For 4 weeks
+                        Calendar calendar3 = Calendar.getInstance();
+                        calendar3.set(selectedYear,selectedMonth,selectedDay);
+                        calendar3.add(calendar3.DATE,28);
+                        int fourWeekD = (calendar3.get(Calendar.DAY_OF_MONTH));
+                        int fourWeekM = calendar3.get(Calendar.MONTH);
+                        fourWeekM=fourWeekM+1;
+                        int fourWeekY = calendar3.get(Calendar.YEAR);
+
+                        //For 6 weeks
+                        Calendar calendar4 = Calendar.getInstance();
+                        calendar4.set(selectedYear,selectedMonth,selectedDay);
+                        calendar4.add(calendar4.DATE,42);
+                        int sixWeekD = (calendar4.get(Calendar.DAY_OF_MONTH));
+                        int sixWeekM = calendar4.get(Calendar.MONTH);
+                        sixWeekM=sixWeekM+1;
+                        int sixWeekY = calendar4.get(Calendar.YEAR);
+
+                        //For 8 weeks
+                        Calendar calendar5 = Calendar.getInstance();
+                        calendar5.set(selectedYear,selectedMonth,selectedDay);
+                        calendar5.add(calendar5.DATE,56);
+                        int eightWeekD = (calendar5.get(Calendar.DAY_OF_MONTH));
+                        int eightWeekM = calendar5.get(Calendar.MONTH);
+                        eightWeekM=eightWeekM+1;
+                        int eightWeekY = calendar5.get(Calendar.YEAR);
 
                         //TextView's Text
                         selectedMonth=selectedMonth+1;
@@ -46,8 +80,12 @@ public class TimeCalcThirdPart extends AppCompatActivity {
                         tvDate.setText(date);
                         //TextView's Text
 
-
-                        date_text.setText("Date is : " + threeWeekD +"/"+threeWeekM+"/"+threeWeekY);
+                        is_uc.setText(threeWeekD + "/" + threeWeekM + "/" + threeWeekY);
+                        is_dort.setText(fourWeekD + "/" + fourWeekM + "/" + fourWeekY);
+                        tuketici_uc.setText(threeWeekD + "/" + threeWeekM + "/" + threeWeekY);
+                        tuketici_dort.setText(fourWeekD + "/" + fourWeekM + "/" + fourWeekY);
+                        ticari_alti.setText(sixWeekD + "/" + sixWeekM + "/" + sixWeekY);
+                        ticari_sekiz.setText(eightWeekD + "/" + eightWeekM + "/" + eightWeekY);
                     }
                 },year,month,day);
                 datePickerDialog.show();
